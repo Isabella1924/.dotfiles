2a6983c6fb09c565b3b08b65f76d398e9e0b0930
Isabella <ita@email.sc.edu>
2023-04-07 22:45:09 -0400

    Created linux shell setup scripts.

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
  
 rm -r ~/.vimrc #remove .vimrc from home dir
-sed -i "/source ∼\/.dotfiles\/bashrc custom/d" ~/.bashrc #replace the pattern with nothing in the .bashrc file
+sed -i "/source ∼\/\.dotfiles\/bashrc_custom/g" ~/.bashrc #replace the pattern with nothing in the .bashrc file
 rm -r ~/.TRASH #remove trash from home dir
